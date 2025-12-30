00830caca4019587e88ee45e2478bd712aebb418
Arham Tahir <146723266+Arhamhir@users.noreply.github.com>
2025-12-30 16:40:04 +0500

    fixxx

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: process.env.VITE_BASE_PATH || "/Obsidian_yellow",
+  base: process.env.VITE_BASE_PATH || "/Arham-Tahir",
   server: {
     host: "::",
     port: 8080,
